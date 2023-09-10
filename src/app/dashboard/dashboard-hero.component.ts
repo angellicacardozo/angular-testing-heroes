8311ce5d7b2e2cb02dcefa62f5c1dd8ecab745be
Angellica Araujo <angellica.c.a@gmail.com>
2023-09-10 14:55:55 -0300

    wip: styles for dashboard

--- src/app/dashboard/dashboard-hero.component.ts
+++ src/app/dashboard/dashboard-hero.component.ts
@@ -5,9 +5,12 @@ import { Hero } from '../model/hero';
 @Component({
   selector: 'dashboard-hero',
   template: `
-    <button type="button" (click)="click()" class="hero">
-      {{hero.name | uppercase}}
-    </button>
+    <div class="flex flex-row gap-2 items-center">
+        <span>{{ hero.name | uppercase }}</span>
+        <a class="border-4 border-white rounded-full p-2 text-xs uppercase tracking-wide text-stone-600 font-semibold" (click)="click()">
+          Details
+        </a>
+    </div>
   `,
   styleUrls: [ './dashboard-hero.component.css' ]
 })
